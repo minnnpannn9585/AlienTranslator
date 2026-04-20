846928100263300f0ab85f32c933057ae4746896
MinPan9585 <panmin677095@gmail.com>
2026-04-20 21:21:23 +0800

    ending panel, bgm, card definition

--- Assets/Scripts/CardDefinition.cs
+++ Assets/Scripts/CardDefinition.cs
@@ -8,9 +8,11 @@ public sealed class CardDefinition : ScriptableObject
     [SerializeField] private Sprite artwork;
     [TextArea(2, 5)]
     [SerializeField] private string contentText;
+    [SerializeField] private bool isAngryCard;
 
     public int Id => id;
     public string DisplayName => displayName;
     public Sprite Artwork => artwork;
     public string ContentText => contentText;
+    public bool IsAngryCard => isAngryCard;
 }
